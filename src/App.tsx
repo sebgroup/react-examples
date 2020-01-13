68b05aba147ed3ba19f4d694f8b724a5732d5c1f
Mario Subotic <mario.subotic@seb.se>
2020-01-13 17:55:28 +0800

    removed default example and added one component with lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { lazy, Suspense } from "react";
 import "./App.scss";
+
+const Main = lazy(() => import("./components/Main"));
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>loading ...</div>}>
+      <div className="container-fluid app-root">
+        <Main />
+      </div>
+    </Suspense>
   );
 };
 
