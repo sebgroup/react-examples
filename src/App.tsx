81664c53cba851fafc86a8ddaf61f2d3c010c1ea
Mario Subotic <mario.subotic@seb.se>
2020-01-14 17:15:07 +0800

    added sidebar component boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React, { lazy, Suspense } from "react";
 import "./App.scss";
 
 const Main = lazy(() => import("./components/Main"));
+const Sidebar = lazy(() => import("./components/Sidebar"));
 
 const App: React.FC = () => {
   return (
+    // TODO: add global loader as Context
+    // TODO: add global notifications as Context
     <Suspense fallback={<div>loading ...</div>}>
       <div className="container-fluid app-root">
+        <Sidebar />
         <Main />
       </div>
     </Suspense>
